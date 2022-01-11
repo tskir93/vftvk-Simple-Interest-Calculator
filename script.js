e3230e3e93bf09cec152b1c09eb1ffec73bae6a1
Kyriakos Tsainis <tsainis.k93@gmail.com>
2022-01-11 20:50:21 +0000

    All the changes made in the App

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //using <mark> in order to highlight the numbers i get
+    document.getElementById("result").innerHTML= "If you deposit <mark>"+ principal +"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark> %\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//new function for validation
+function validatePrincipal()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal <=0 ) {
+      alert("Enter a positive number"); 
+    }
+    //using focus function after alert message to focus on principal value
+    document.getElementById("principal").focus();
+  }
